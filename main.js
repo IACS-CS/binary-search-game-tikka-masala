1ec8d6812eb9b39e91abbb2a77a66eecdfe4ffba
Tron <109302537+MrDonaldGlov@users.noreply.github.com>
2024-09-10 14:34:11 +0000

    Added title (Cool guys)

--- main.js
+++ main.js
@@ -3,8 +3,5 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface");
-ti.output("What is your name?");
-let name = await ti.readText();
-ti.output("Hello, " + name);
-
+let ti = new TextInterface(app, " Family Guy Quiz");
+ti.output("Would you like to play a game?");
