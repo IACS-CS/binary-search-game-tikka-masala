5841f02345c4f6504dac97181b158c3e93f16a3c
rmamet <179627394+rmamet@users.noreply.github.com>
2024-09-10 14:38:07 +0000

    Changed Title, and change
    Co-authored-by: EnosTrain <EnosTrain@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface");
-ti.output("What is your name?");
-let name = await ti.readText();
-ti.output("Hello, " + name);
-
+let ti = new TextInterface(app, "What gravity falls character are you?");
+ti.output("Welcome! Lets find out what gravity falls character you are!");
+ti.output(
+  await ti.readChoice(["Option 1", "Option 2"], "Which one do you prefer?")
+);
