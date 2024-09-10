af16640fdebcab3e3a0bd9d1064dba769c617288
cee1904 <itscece890@gmail.com>
2024-09-10 14:34:20 +0000

    changed title- cik, sk, sb

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface");
+let ti = new TextInterface(app, "Guess The Hogwarts House");
 ti.output("What is your name?");
 let name = await ti.readText();
 ti.output("Hello, " + name);
