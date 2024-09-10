5a8037e1e2563d7b69efd343f5ef04e1e035340f
rmamet <179627394+rmamet@users.noreply.github.com>
2024-09-10 15:00:26 +0000

    Add first questions - RM / XE

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 import "./style.css";
 import { TextInterface } from "text-interface";
 
+let binary = 0;
+
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
 let ti = new TextInterface(app, "What gravity falls character are you?");
 ti.output("Welcome! Lets find out what gravity falls character you are!");
-ti.output(
-  await ti.readChoice(["Option 1", "Option 2"], "Which one do you prefer?")
-);
+ti.output("Do you have a job?");
+answer = await ti.readYesOrNo()
+if (answer){
+  ti.output("Are you tall?");
+  answer = await ti.readYesOrNo()
+} else if (!answer) {
+  ti.output("Do you prefer to stay in the same place?");
+  answer = await ti.readYesOrNo()
+} else {
+  ti.output("idk what to tell you, man. It broke")
+}
