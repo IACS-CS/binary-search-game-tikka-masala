b298c9b4c152b36d21ee18a5604c861529de091d
cee1904 <itscece890@gmail.com>
2024-09-10 14:56:57 +0000

    first response- cik, sk, sb

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let app = document.querySelector("#app");
 // Create a new "Text Interface"
 let ti = new TextInterface(app, "Guess Your Hogwarts House");
 ti.output("Are you brave?");
-let name = await ti.readText();
-ti.output("Hello, " + name);
-
+let brave = await ti.readYesOrNo();
+if (brave) {
+  ti.output("You are a Gryffindor!")
+}
