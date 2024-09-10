5aace215decccc4ae26a7b63cd9e953bd5301cca
Andrecoder1234 <rocketpirate33@gmail.com>
2024-09-10 15:00:11 +0000

    Intro AP

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import "./style.css";
 import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
-// Create a new "Text Interface"
 let ti = new TextInterface(app, "Which soccer player are you?");
-ti.output("What is your name?");
-let name = await ti.readText();
-ti.output("Hello, " + name);
+ti.output("Hello and welcomeeee to Which soccer play are you.");
+ti.output("My name is Bob I'll be your host I hope your ready");
+let userName = await ti.readText();
+ti.output("It is lovely to meet you, " + userName);
+ti.output("whats your favorite number?");
+await ti.readText();
