0f8ee0c367eb08c3df1717d84054db6b24891179
Mason2435 <cayermason8@gmail.com>
2024-09-10 14:37:54 +0000

    king

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Mason's, Guess The Sport");
+let ti = new TextInterface(app, "king allan's Guess The Sport");
 ti.output("What is your name?");
 let name = await ti.readText();
 ti.output("Hello, " + name);
