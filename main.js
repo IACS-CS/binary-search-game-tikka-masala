32e54ad55f6cafceb7d2e327d072718528618626
NRpatel002 <dev.patel@innovationcharter.org>
2024-09-12 13:25:09 +0000

    MP,DP,CB/ made quotions

--- main.js
+++ main.js
@@ -14,10 +14,31 @@ let ti = new TextInterface(app, "What chocolate bar are you?");
 ti.output("What is your name?");
 let name = await ti.readText();
 ti.output("Hello, " + name);
-
-
+const choices = ["Math", "English", "science","History"];
+  let selection = await ti.readChoice(
+    choices,
+    // list of choices
+    "What is your favorite class?",
+    // prompt (optional)
+    "I like that class"
+    // custom error message (optional)
+  )
+  const seasons = ["Spring", "Summer", "Fall", "Winter"];
+  season = await ti.readChoice(
+    seasons,
+    // list of choices
+    "What is your favorite season?",
+    // prompt (optional)
+    "Fall is the better season"
+    // custom error message (optional)
+  );
+  if (selection == "Fall") {
+    ti.output("Chocolate is my favorite too!");
+  } else {
+    ti.output(`Ok, I guess. Fall is better, but I like ${selection} too.`);
+  }
 //what is your fev class.(english,math,science,history)
-//what is your fec season (fall.winter,spring,summer)
+//what is your fev season (fall.winter,spring,summer)
 //What country do you want to visit(dubai,aus,japen,italy,)
 //what is your fav games(uno,mon,chess,scrabble)
 //what is your gaming platform(pc,ps4,xbox,switch)
