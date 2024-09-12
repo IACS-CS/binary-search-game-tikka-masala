f704403c82a8187b9e07d6ddb2f938e9e0a6cdc2
NRpatel002 <dev.patel@innovationcharter.org>
2024-09-12 12:46:31 +0000

    latest

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ let ti = new TextInterface(app, "What chocolate bar are you?");
 ti.output("What is your name?");
 let name = await ti.readText();
 ti.output("Hello, " + name);
+
 //what is your fev class.(english,math,science,history)
 //what is your fec season (fall.winter,spring,summer)
 //What country do you want to visit(dubai,aus,japen,italy,)
 //what is your fav games(uno,mon,chess,scrabble)
 //what is your gaming platform(pc,ps4,xbox,switch)
+//what is your fav color?(red,blue,pink,purple)
